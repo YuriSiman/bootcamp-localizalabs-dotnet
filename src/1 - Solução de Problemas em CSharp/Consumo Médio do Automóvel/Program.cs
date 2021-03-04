7e666bf7b89ff499e83b9366e159c3f7f5c15567
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-04 13:34:10 -0300

    Projeto Consumo Médio do Automóvel

---
+++ src/1 - Solução de Problemas em CSharp/Consumo Médio do Automóvel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Consumo_Médio_do_Automóvel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double combustivelGasto, consumoMedio;
+
+            Console.Write("Informe a distância: ");
+            distancia = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o combustível gasto: ");
+            combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+            consumoMedio = distancia / combustivelGasto;
+
+            Console.WriteLine("O consumo médio é de {0:0.000} km/l", consumoMedio);
+        }
+    }
+}
