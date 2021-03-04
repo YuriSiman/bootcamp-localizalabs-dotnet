08e1788cc551fbfdef498b5be8e9182cadaf44bc
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-04 13:45:17 -0300

    Projeto Média

---
+++ src/3 - Desafios Aritméticos em CSharp/Média/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Média
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, mediaP;
+
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+
+            a = Convert.ToDouble(a.ToString("F1"));
+            b = Convert.ToDouble(b.ToString("F1"));
+
+            mediaP = ((a * 3.5) + (b * 7.5)) / (3.5 + 7.5);
+
+            Console.WriteLine("MEDIA = {0}", mediaP.ToString("0.00000", CultureInfo.InvariantCulture));
+        }
+    }
+}
