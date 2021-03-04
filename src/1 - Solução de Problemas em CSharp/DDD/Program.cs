ee2b28cd274eb3ea114cef1c77b0738af3e36666
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-04 13:36:01 -0300

    Projeto DDD

---
+++ src/1 - Solução de Problemas em CSharp/DDD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o DDD: ");
+            int ddd = Convert.ToInt32(Console.ReadLine());
+
+            switch (ddd)
+            {
+                case 61:
+                    Console.WriteLine("Brasilia");
+                    break;
+                case 71:
+                    Console.WriteLine("Salvador");
+                    break;
+                case 11:
+                    Console.WriteLine("Sao Paulo");
+                    break;
+                case 21:
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+                case 32:
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+                case 19:
+                    Console.WriteLine("Campinas");
+                    break;
+                case 27:
+                    Console.WriteLine("Vitoria");
+                    break;
+                case 31:
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+                default:
+                    Console.WriteLine("DDD nao cadastrado");
+                    break;
+            }
+        }
+    }
+}
