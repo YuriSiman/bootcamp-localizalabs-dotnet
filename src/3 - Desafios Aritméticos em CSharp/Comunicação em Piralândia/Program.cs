1ff8bc46bccf093a69677edbcd59478fa746d9de
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-04 20:17:29 -0300

    Projeto Comunicação em Piralândia

---
+++ src/3 - Desafios Aritméticos em CSharp/Comunicação em Piralândia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Comunicação_em_Piralândia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+
+            char[] arr = n.ToCharArray();
+            Array.Reverse(arr);
+
+            string v = new string(arr);
+            Console.WriteLine(v);
+        }
+    }
+}
