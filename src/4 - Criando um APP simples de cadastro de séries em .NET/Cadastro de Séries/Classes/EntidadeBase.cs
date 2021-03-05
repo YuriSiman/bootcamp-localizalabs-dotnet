80f68b0542b53b377bed7b1e68fdfe06f6101e64
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-04 22:59:15 -0300

    Projeto Cadastro de Séries

---
+++ src/4 - Criando um APP simples de cadastro de séries em .NET/Cadastro de Séries/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Cadastro_de_Séries.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ src/4 - Criando um APP simples de cadastro de séries em .NET/Cadastro de Séries/Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+﻿using Cadastro_de_Séries.Interfaces;
+using System.Collections.Generic;
+
+namespace Cadastro_de_Séries.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
